353ae963ccc3b97ccaed4bc29e3a52cb9c6f7596
Halogen96 <rachitnimje9124@gmail.com>
2024-07-11 22:35:24 +0530

    added logout controller

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,28 @@
+import { User } from "../models/user.models";
+import { apiError } from "../utils/apiError";
+import { asyncHandler } from "../utils/asyncHandler";
+import jwt from "jsonwebtoken"
+
+export const verifyJWT = asyncHandler( async (req, _, next) => {
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+    
+        if(!token) {
+            throw new apiError(401, "Unauthorized access!")
+        }
+    
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    
+        const user = await User.findById(decodedToken._id).select("-password -refreshToken")
+        
+        if (!user) {
+            throw new apiError(401, "Invalid access token!")
+        }
+
+        req.user = user
+        next()
+
+    } catch (error) {
+        throw new apiError(401, error?.message || "Invalid access token!")
+    }
+})
